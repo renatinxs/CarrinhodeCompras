f4621382a84348a9f4826598e632f86d76ad99cf
Samuel B <sam1219barbosa@gmail.com>
2026-05-01 18:33:08 -0300

    Finalizando projeto

--- CarrinhodeCompras/Program.cs
+++ CarrinhodeCompras/Program.cs
@@ -7,13 +7,42 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpContextAcessor();
 
+//Adicionar a interface como serviço
 builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context +> true;
+    options.MinimumSameSitePolicy = SameSiteMode.none;
+});
+
+// Corrigir problema com TEMPDATA para aumentar o tempo de duração
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    // Set a short timeout for easy testing.
+    options.IdleTimeout = TimeSpan.FromSeconds(900);
+    options.Cookie.HttpOnly = true;
+    // Deixar informado para o navegador que a sessão é essencial
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddMvc().AddSessionStateTempDataProvider();
+
+builder.Services.AddMemoryCache(); // Guardar os dados na memoria
+
+//Add Gerenciador Arquivo como serviços
+builder.Services.AddScoped<GerenciadorArquivo>();
+builder.Services.AddScoped<ProjetoEmprestimo.Cookie.Cookie>();
+builder.Services.AddScoped<ProjetoEmprestimo.CarrinhoCompra.CookieCarrinhoCompra>();
 
 var app = builder.Build();
 
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
